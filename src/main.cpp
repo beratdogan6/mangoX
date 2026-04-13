01563b049a3231c24701aa1c4d27c273b5df85a4
beratdogan6 <berat.dogan.3458@gmail.com>
2026-04-13 16:28:08 +0300

    opengl window setup with glfw and glad

---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include <cstdio>
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+
+void error_callback(int error, const char *description) {
+    fprintf(stderr, "GLFW Error (%d): %s\n", error, description);
+}
+
+void framebuffer_size_callback(GLFWwindow * /*window*/, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+int main() {
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "mangoX", NULL, NULL);
+    if (window == NULL) {
+        fprintf(stderr, "Failed to create GLFW window\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        fprintf(stderr, "Failed to initialize GLAD\n");
+        return -1;
+    }
+
+    glViewport(0, 0, 1280, 720);
+
+    while (!glfwWindowShouldClose(window)) {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, true);
+
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
